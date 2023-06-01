257b5b0f3047911d95c27eb5b5bd78b68a050ff4
changheon woo <changheon.woo@gmail.com>
2023-06-01 11:17:07 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 st.write("""
 # My first app
 Hello *world!*
 """)
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
